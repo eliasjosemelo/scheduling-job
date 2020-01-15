fbe1a522710a81fea97fbfbfb09b6505bf9702d3
Elias José Paulino de Melo <eliasjosemelo@gmail.com>
2020-01-15 11:15:00 -0300

    Adicionada função para ordenar os jobs conforme data máxima de execução

--- src/job-grouper/job-grouper.js
+++ src/job-grouper/job-grouper.js
@@ -6,9 +6,24 @@
 'use strict';
 
 function group (jobs) {
+  
+  let sorted = sortByMaxConclusionDate(jobs);
+  
   return [];
 };
 
+function sortByMaxConclusionDate (jobs) {
+  return jobs.sort((x, y) => {
+    
+    if (x.maxConclusionDate < y.maxConclusionDate) {
+      return -1;
+    }
+    
+    return 1;
+  });
+}
+
 module.exports = {
-    group: group
+    group: group,
+    sortByMaxConclusionDate: sortByMaxConclusionDate
 };
--- src/test/job-grouper-test.js
+++ src/test/job-grouper-test.js
@@ -6,10 +6,69 @@
 'use strict';
 
 const chai = require('chai');
+
 const jobGrouper = require('../job-grouper/job-grouper');
+
+const testData = [
+    {
+      "input": [
+        {
+          "id": 1,
+          "description": "Importação de arquivos de fundos",
+          "maxConclusionDate": new Date(2019, 11, 10, 12, 0, 0, 0),
+          "duration": 2,
+        },
+        {
+          "id": 2,
+          "description": "Importação de dados da Base Legada",
+          "maxConclusionDate": new Date(2019, 11, 11, 12, 0, 0, 0),
+          "duration": 4,
+        },
+        {
+          "id": 3,
+          "description": "Importação de dados de integração",
+          "maxConclusionDate": new Date(2019, 11, 11, 08, 0, 0, 0),
+          "duration": 6,
+        }
+      ],
+      "output" :[
+        {
+          "id": 1,
+          "description": "Importação de arquivos de fundos",
+          "maxConclusionDate": new Date(2019, 11, 10, 12, 0, 0, 0),
+          "duration": 2,
+        },
+        {
+          "id": 3,
+          "description": "Importação de dados de integração",
+          "maxConclusionDate": new Date(2019, 11, 11, 08, 0, 0, 0),
+          "duration": 6,
+        },
+        {
+          "id": 2,
+          "description": "Importação de dados da Base Legada",
+          "maxConclusionDate": new Date(2019, 11, 11, 12, 0, 0, 0),
+          "duration": 4,
+        }
+      ]
+    }
+];
+
 
 const assert = chai.assert;
 
-it('Retorna um "Array"', () => {
-  assert.isArray(jobGrouper.group());
+describe('Validações de Ordenação', () => {
+  
+  it('Deve ordenar o array por maxConclusionDate crescente', () => {
+    assert.deepEqual(jobGrouper.sortByMaxConclusionDate(testData[0].input), testData[0].output);
+  });
+  
+});
+
+describe('Validações de Retorno', () => {
+
+  it('Retorna um "Array"', () => {
+    assert.isArray(jobGrouper.group([]));
+  });
+
 });
