caafcc442da5d3e785730e5aa4ace5f084705bd0
Elias José Paulino de Melo <eliasjosemelo@gmail.com>
2020-01-17 09:35:21 -0300

    Adicionado tratamento e testes para verificar a janela de execução.

--- src/job-grouper/job-grouper.js
+++ src/job-grouper/job-grouper.js
@@ -6,22 +6,30 @@
 "use strict";
 
 function prepare (jobs, windowStartDate, windowEndDate) {
-  let windowSize = 8;
+  // cálculo do tamanho da janela em horas
+  let windowSize = (windowEndDate - windowStartDate) / 1000 / 60 / 60;
+  // tamanho da janela de agrupamento
+  let groupWindowSize = 8;
   
   try {
     
     let sorted = sortByMaxConclusionDate(jobs);
     
-    return group(sorted, windowSize);
+    return group(sorted, groupWindowSize, windowSize);
     
   } catch (e) {
     throw "Parameter jobs needs to be an array of objects";
   }
 };
 
-function group (jobs, windowSize) {
+function group (jobs, groupWindowSize, windowSize) {
+  //objeto a ser retornado
   let groupment = [];
+  //array utiizado para o agrupamento
   let windowGroupment = [];
+  //controle para quebrar o agrupamento
+  let windowGroupmentSizeAux = 0;
+  //controle para não ultrapassar o tamanho máximo da janela
   let windowSizeAux = 0;
   
   for (let i = 0, length = jobs.length; i < length; i++)
@@ -29,19 +37,29 @@ function group (jobs, windowSize) {
     
     let job = jobs[i];
     
+    //se for ultrapassar tamanho da janela interrompe o laço
     if ((windowSizeAux + job.duration) > windowSize) {
-      groupment.push(windowGroupment);
-      windowGroupment = [];
-      windowSizeAux = 0;
+      break;
     }
     
+    //se for ultrapassar o tamanho  do agrupamento realiza a quebra
+    if ((windowGroupmentSizeAux + job.duration) > groupWindowSize) {
+      groupment.push(windowGroupment);
+      windowGroupment = [];
+      windowGroupmentSizeAux = 0;
+    }
+    
+    //adiciona o job ao agrupamento e soma os tempos de duração aos controles
     windowGroupment.push(job.id);
+    windowGroupmentSizeAux += job.duration;
     windowSizeAux += job.duration;
   }
   
+  //adiciona o último agrupamento, caso exista
   if (windowGroupment.length > 0) {
       groupment.push(windowGroupment);
       windowGroupment = [];
+      windowGroupmentSizeAux = 0;
       windowSizeAux = 0;
   }
   
--- src/test/job-grouper-test.js
+++ src/test/job-grouper-test.js
@@ -64,7 +64,7 @@ describe("Validações de Ordenação", () => {
 
 describe("Validações de Agrupamento", () => {
 
-  it("Deve retornar um array de arrays de id\"s, agrupando os jobs de forma que a soma das horas de duração sejam no máximo 8.", () => {
+  it("Deve retornar um array de arrays de id\"s, agrupando os jobs de forma que a soma das horas de duração sejam no máximo 8 e respeitar a janela máxima de execução.", () => {
     assert.deepEqual(
       jobGrouper.group([
         {
@@ -215,7 +215,7 @@ describe("Validações de Agrupamento", () => {
       
   });
   
-  it("Deve retornar um array de arrays de id\"s, agrupando os jobs de forma que a soma das horas de duração sejam no máximo 5.", () => {
+  it("Deve retornar um array de arrays de id\"s, agrupando os jobs de forma que a soma das horas de duração sejam no máximo 5 e respeitar a janela máxima de execução.", () => {
     assert.deepEqual(
       jobGrouper.group([
         {
@@ -236,7 +236,7 @@ describe("Validações de Agrupamento", () => {
           "maxConclusionDate": new Date(2019, 11, 11, 08, 0, 0, 0),
           "duration": 5,
         }
-      ], 5),
+      ], 5, 10),
       [
         [1, 2],
         [3]
@@ -263,7 +263,7 @@ describe("Validações de Agrupamento", () => {
           "maxConclusionDate": new Date(2019, 11, 11, 08, 0, 0, 0),
           "duration": 3,
         }
-      ], 5)
+      ], 5, 12)
       [
         [1],
         [2],
@@ -291,7 +291,7 @@ describe("Validações de Agrupamento", () => {
           "maxConclusionDate": new Date(2019, 11, 11, 08, 0, 0, 0),
           "duration": 2,
         }
-      ], 5),
+      ], 5, 8),
       [
         [1],
         [2, 3]
@@ -324,11 +324,81 @@ describe("Validações de Agrupamento", () => {
           "maxConclusionDate": new Date(2019, 11, 11, 08, 0, 0, 0),
           "duration": 4,
         }
-      ], 5),
+      ], 5, 10),
       [
         [1, 2],
         [3],
         [4]
+      ]
+    );
+      
+    assert.deepEqual(
+      jobGrouper.group([
+        {
+          "id": 1,
+          "description": "Importação de arquivos de fundos",
+          "maxConclusionDate": new Date(2019, 11, 10, 12, 0, 0, 0),
+          "duration": 1,
+        },
+        {
+          "id": 2,
+          "description": "Importação de dados da Base Legada",
+          "maxConclusionDate": new Date(2019, 11, 11, 12, 0, 0, 0),
+          "duration": 2,
+        },
+        {
+          "id": 3,
+          "description": "Importação de dados de integração",
+          "maxConclusionDate": new Date(2019, 11, 11, 08, 0, 0, 0),
+          "duration": 3,
+        },
+        {
+          "id": 4,
+          "description": "Processamento de dados diários para relatório",
+          "maxConclusionDate": new Date(2019, 11, 11, 08, 0, 0, 0),
+          "duration": 4,
+        }
+      ], 5, 10),
+      [
+        [1, 2],
+        [3],
+        [4]
+      ]
+    );
+    
+  });
+  
+  it("Deve retornar um array de arrays de id\"s, agrupando os jobs de forma que a soma das horas de duração sejam no máximo 5 e respeitar a janela máxima de execução, deixando o último job fora da lista.", () => {
+    assert.deepEqual(
+      jobGrouper.group([
+        {
+          "id": 1,
+          "description": "Importação de arquivos de fundos",
+          "maxConclusionDate": new Date(2019, 11, 10, 12, 0, 0, 0),
+          "duration": 1,
+        },
+        {
+          "id": 2,
+          "description": "Importação de dados da Base Legada",
+          "maxConclusionDate": new Date(2019, 11, 11, 12, 0, 0, 0),
+          "duration": 2,
+        },
+        {
+          "id": 3,
+          "description": "Importação de dados de integração",
+          "maxConclusionDate": new Date(2019, 11, 11, 08, 0, 0, 0),
+          "duration": 3,
+        },
+        {
+          "id": 4,
+          "description": "Processamento de dados diários para relatório",
+          "maxConclusionDate": new Date(2019, 11, 11, 08, 0, 0, 0),
+          "duration": 4,
+        }
+      ], 5, 9),
+      [
+        [1, 2],
+        [3]
       ]
     );
       
@@ -363,7 +433,7 @@ describe("Validações de Retorno", () => {
     assert.isArray(jobGrouper.prepare([]));
   });
   
-  it("Deve retornar um array de arrays de id\"s, agrupando os jobs de forma que a soma das horas de duração sejam no máximo 8 e orderando por data máxima de conclusão", () => {
+  it("Deve retornar um array de arrays de id\"s, agrupando os jobs de forma que a soma das horas de duração sejam no máximo 8 e orderando por data máxima de conclusão e respeitando as datas de início e fim da janela de execução", () => {
     
     assert.deepEqual(
       jobGrouper.prepare([
@@ -385,7 +455,7 @@ describe("Validações de Retorno", () => {
           "maxConclusionDate": new Date(2019, 11, 11, 08, 0, 0, 0),
           "duration": 6,
         }
-      ]),
+      ], new Date(2019, 11, 10, 8, 0, 0, 0), new Date(2019, 11, 11, 16, 0, 0, 0)),
       [
         [1, 3],
         [2]
@@ -424,11 +494,51 @@ describe("Validações de Retorno", () => {
           "maxConclusionDate": new Date(2019, 11, 12, 08, 0, 0, 0),
           "duration": 5,
         }
-      ]),
+      ], new Date(2019, 11, 10, 2, 0, 0, 0), new Date(2019, 11, 11, 6, 0, 0, 0)),
       [
         [3],
         [2, 4],
         [5, 1]
+      ]
+    );
+    
+    assert.deepEqual(
+      jobGrouper.prepare([
+        {
+          "id": 1,
+          "description": "Importação de arquivos de fundos",
+          "maxConclusionDate": new Date(2019, 11, 12, 12, 0, 0, 0),
+          "duration": 2,
+        },
+        {
+          "id": 2,
+          "description": "Importação de dados da Base Legada",
+          "maxConclusionDate": new Date(2019, 11, 11, 12, 0, 0, 0),
+          "duration": 4,
+        },
+        {
+          "id": 3,
+          "description": "Importação de dados de integração",
+          "maxConclusionDate": new Date(2019, 11, 10, 08, 0, 0, 0),
+          "duration": 6,
+        },
+        {
+          "id": 4,
+          "description": "Importação de dados de integração",
+          "maxConclusionDate": new Date(2019, 11, 11, 16, 0, 0, 0),
+          "duration": 3,
+        },
+        {
+          "id": 5,
+          "description": "Importação de dados de integração",
+          "maxConclusionDate": new Date(2019, 11, 12, 08, 0, 0, 0),
+          "duration": 5,
+        }
+      ], new Date(2019, 11, 10, 2, 0, 0, 0), new Date(2019, 11, 10, 20, 0, 0, 0)),
+      [
+        [3],
+        [2, 4],
+        [5]
       ]
     );
     
